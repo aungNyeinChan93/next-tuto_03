4f4145794da85d00f143b45d44e36c2498e2432b
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-07-26 14:47:15 +0630

    tuto: use parmas and searchParams in severcomponent

--- src/app/(home)/product/[productId]/review/[reviewId]/page.tsx
+++ src/app/(home)/product/[productId]/review/[reviewId]/page.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
 
 interface ProductReviewProps {
-    params: Promise<{
-        reviewId: string,
-        productId: string
-
-    }>
+  params: Promise<{
+    reviewId: string;
+    productId: string;
+  }>;
+  searchParams: Promise<{
+    lang?: "en" | "jpn" | "mm" | string;
+  }>;
 }
 
-const ProductReview: React.FC<ProductReviewProps> = async ({ params }) => {
-    const { reviewId, productId } = await params
-    return (
-        <React.Fragment>
-            <section>{`Review Id - ${reviewId}`} for product -{productId}</section>
-        </React.Fragment>
-    );
+const ProductReview: React.FC<ProductReviewProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const { reviewId, productId } = await params;
+  const { lang = "mm" } = await searchParams;
+  return (
+    <React.Fragment>
+      <section>
+        {`Review Id - ${reviewId}`} for product -{productId}||
+        {lang && `language -  ${lang}`}
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default ProductReview;
