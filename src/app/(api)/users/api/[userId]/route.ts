88fb653af6a4f006710513bfc944b09941a24893
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-07-27 20:58:04 +0630

    test:update user api

--- src/app/(api)/users/api/[userId]/route.ts
+++ src/app/(api)/users/api/[userId]/route.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { users } from "../data"
 
-// getuser
-export async function GET(request: Request, { params }: { params: Promise<{ userId: string }> }) {
+// method:GET , endpoint:users/api/{userId} , action:getuser
+export async function GET(_request: Request, { params }: { params: Promise<{ userId: string }> }) {
     try {
         const { userId } = await params;
         const user = users.find((user: any) => user.id === Number(userId))
@@ -10,4 +10,12 @@ export async function GET(request: Request, { params }: { params: Promise<{ user
     } catch (error) {
         console.error(error)
     }
+}
+
+// method:PATCH , endpoint:/users/api/{userId} , action:updateUser
+export async function PATCH(request: Request, { params }: { params: Promise<{ userId: string }> }) {
+    const { userId } = await params;
+    const bodyData = await request.json();
+    const usersData = users && users.map((user: any) => user.id === Number(userId) ? { ...user, ...bodyData } : user)
+    return Response.json({ bodyData, usersData });
 }
