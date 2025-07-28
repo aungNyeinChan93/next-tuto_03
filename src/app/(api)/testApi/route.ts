edc4835cf3138362b07654fbbd6259cbb0abfa49
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-07-28 11:26:14 +0630

    test: cookie and header

--- src/app/(api)/testApi/route.ts
+++ src/app/(api)/testApi/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 // Get header Data in next js!!
@@ -12,15 +12,36 @@ import { NextRequest } from "next/server";
 // }
 
 // >>> sec way
-export async function GET() {
-    const header = await headers();
+export async function GET(request: NextRequest) {
+
+    const header = await headers();  // next header
     const auth = header.get('Authorization');
     const name = header.get('name');
 
-    // return Response.json({ auth, name })
-    return new Response(`<h1>${JSON.stringify({ auth, name })}</h1>`, {
+    // get cookie
+    const theme = request.cookies.get('theme')
+    const cookieName = request.cookies.get('name');
+
+    const cookieStorage = await cookies(); //next cookie
+    cookieStorage.set('username', "aung nyein chan")
+
+    // delete cookie
+    // cookieStorage.delete('theme')
+
+    return Response.json(
+        {
+            auth,
+            name,
+            theme: theme?.value,
+            cookieName: cookieName,
+            username: cookieStorage.get('username'),
+        })
+
+    return new Response('header and cookies', {
+        // set tradational response header and set cookies
         headers: {
-            'Content-type': 'text/html'
+            'Content-type': 'application/json',
+            'Set-Cookie': 'theme=night',
         }
     })
 }
