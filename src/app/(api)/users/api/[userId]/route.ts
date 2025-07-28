31665b804464f93257f664ebb65f85e274e4aa9c
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-07-28 09:55:49 +0630

    test:crud users api

--- src/app/(api)/users/api/[userId]/route.ts
+++ src/app/(api)/users/api/[userId]/route.ts
@@ -16,6 +16,15 @@ export async function GET(_request: Request, { params }: { params: Promise<{ use
 export async function PATCH(request: Request, { params }: { params: Promise<{ userId: string }> }) {
     const { userId } = await params;
     const bodyData = await request.json();
-    const usersData = users && users.map((user: any) => user.id === Number(userId) ? { ...user, ...bodyData } : user)
+    const usersData = users && userId && users.map((user: any) => user.id === Number(userId) ? { ...user, ...bodyData } : user)
     return Response.json({ bodyData, usersData });
+}
+
+// method:Delete , endpoint:/users/api/{userId} , action:deleteUser
+export async function DELETE(_request: Request, { params }: { params: Promise<{ userId: string }> }) {
+    const { userId } = await params;
+    const index: any = users && userId && users.findIndex(user => user.id === Number(userId))
+    const deleteUser = users[index];
+    users.splice(index, 1)
+    return Response.json({ deleteUser, index, users })
 }
--- src/app/(api)/users/api/route.ts
+++ src/app/(api)/users/api/route.ts
@@ -1,10 +1,11 @@
+import { NextRequest } from "next/server";
 import { users } from "./data";
 
 // all users
-export async function GET() {
-    const test = await Promise.resolve('what is that?');
-    if (test) return Response.json({ users, test });
-};
+// export async function GET() {
+//     const test = await Promise.resolve('what is that?');
+//     if (test) return Response.json({ users, test });
+// };
 
 // create user 
 export async function POST(request: Request) {
@@ -12,6 +13,19 @@ export async function POST(request: Request) {
     user.id = users.length + 1;
     users.push(user);
     return new Response(JSON.stringify(user), { status: 201 })
+}
+
+// getUsersByName and byGender , endpoint-> /users/api/ , params ->name=string or gender=male|female
+export async function GET(request: NextRequest) {
+    const { searchParams } = request.nextUrl;
+    const name = searchParams.get('name');
+    const gender = searchParams.get('gender');
+    const serachUsers = name
+        ? users.find(user => user.name.includes(name))
+        : gender
+            ? users.filter(user => user.gender === gender)
+            : users
+    if (serachUsers) return Response.json({ serachUsers })
 }
 
 
